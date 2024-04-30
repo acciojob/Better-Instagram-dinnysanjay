fc2a059db7f510fdab0dedf520e6f49c774a8e40
dinnysanjay <160900894+dinnysanjay@users.noreply.github.com>
2024-04-30 19:47:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//your code here
 let dragElem = null;
 
 // Get all images
@@ -10,20 +9,19 @@ for(let i=0; i<images.length; i++) {
         console.log("dragging started", e);
         dragElem = e.target;
     });
+    
+    images[i].addEventListener("dragover", e => {
+        e.preventDefault(); // This is necessary to allow dropping
+    });
+
+    images[i].addEventListener("drop", e => {
+        e.preventDefault(); // This is necessary to allow dropping
+
+        // Swap the images of the dragged element and the target element
+        let temp = e.target.src;
+        e.target.src = dragElem.src;
+        dragElem.src = temp;
+
+        dragElem = null;
+    });
 }
-
-// Add event listener to the parent
-document.getElementById("parent").addEventListener("dragover", e => {
-    e.preventDefault(); // This is necessary to allow dropping
-});
-
-document.getElementById("parent").addEventListener("drop", e => {
-    e.preventDefault(); // This is necessary to allow dropping
-    
-    // Swap the images of the dragged element and the target element
-    let temp = e.target.src;
-    e.target.src = dragElem.src;
-    dragElem.src = temp;
-    
-    dragElem = null;
-});
