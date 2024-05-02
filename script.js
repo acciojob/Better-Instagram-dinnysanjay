1ec3591f15e57a37dd47de7a04cab2fb8b63d9fa
dinnysanjay <160900894+dinnysanjay@users.noreply.github.com>
2024-05-02 21:09:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 let dragElem = null;
+let temp = e.target.style.backgroundImage;
+e.target.style.backgroundImage = dragElem.style.backgroundImage;
+dragElem.style.backgroundImage = temp;
+//This should correctly swap the images, but there may be an issue if e.target is not the div that you’re expecting. For example, if the divs contain other elements, e.target could be one of those child elements, not the div itself.
+
+//To ensure that you’re always working with the divs, you can use the closest method to get the nearest ancestor element that matches a given selector. In your case, you can use it to always get the div:
+
+let div = e.target.closest('.image');
+
+let temp = div.style.backgroundImage;
+div.style.backgroundImage = dragElem.style.backgroundImage;
+dragElem.style.backgroundImage = temp;
 
 function addEventListeners(div) {
     div.addEventListener("dragstart", e => {
